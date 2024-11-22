1a2f895aa9a25b9a3f9822c4f478c12907309903
Saeed <yasrebi.saeed@gmail.com>
2024-11-22 18:10:09 +0000

    adding config module

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -5,7 +5,7 @@ import secrets
 secret_key = secrets.token_urlsafe(32)
 
 class Config:
-
+    #############Basic configuration ###################
     ENV = environ.get("BACKEND_ENV", "development")
 
     DEBUG = bool(int(environ.get("BACKEND_DEBUG", "0")))
